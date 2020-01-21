94d7d56b0086d6de80d8be3579731c09cc409062
Nathaniel Bond <nabond251@gmail.com>
2020-01-21 01:28:40 +0000

    Initial commit
    Add initial working pump demo, a la
    https://www.electronicwings.com/raspberry-pi/
    raspberry-pi-pwm-generation-using-python-and-c

---
+++ gardener.cpp
@@ -0,0 +1,61 @@
+/*
+ Change the Intensity of LED using PWM on Raspberry Pi
+ http://www.electronicwings.com
+ */
+
+#include <wiringPi.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int PWM0_pin = 1; /* GPIO 1 as per WiringPi, GPIO18 as per BCM */
+const int PWM1_pin = 24;
+
+int main(void)
+{
+  int intensity;
+
+  if (wiringPiSetup() == -1)
+  {
+    exit(1);
+  }
+
+  pinMode(PWM0_pin, PWM_OUTPUT); /* set PWM pin as output */
+  pinMode(PWM1_pin, PWM_OUTPUT);
+
+  while (1)
+  {
+    for (intensity = 0; intensity < 1024; intensity++)
+    {
+      pwmWrite(PWM0_pin, intensity); /* provide PWM value for duty cycle */
+      delay(1);
+    }
+
+    delay(10000);
+
+    for (intensity = 1023; intensity >= 0; intensity--)
+    {
+      pwmWrite(PWM0_pin, intensity);
+      delay(1);
+    }
+
+    delay(1);
+#if 1
+    for (intensity = 0; intensity < 1024; intensity++)
+    {
+      pwmWrite(PWM1_pin, intensity); /* provide PWM value for duty cycle */
+      delay(1);
+    }
+
+    delay(10000);
+
+    for (intensity = 1023; intensity >= 0; intensity--)
+    {
+      pwmWrite(PWM1_pin, intensity);
+      delay(1);
+    }
+
+    delay(1);
+#endif
+  }
+}
